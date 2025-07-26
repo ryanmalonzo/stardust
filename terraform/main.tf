a34f1a9506d05d7451d27d9d842e4bd953e9170a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-07-26 23:32:08 +0200

    feat(terraform): add docker vm configuration and outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "proxmox" {
   insecure = true
 }
 
-data "proxmox_virtual_environment_datastores" "terraform" { 
+data "proxmox_virtual_environment_datastores" "terraform" {
   node_name = var.proxmox_node_name
   filters = {
     id = "terraform"
@@ -36,4 +36,59 @@ resource "proxmox_virtual_environment_file" "docker_cloud_init" {
 
     file_name = "docker-cloud-init.yaml"
   }
+}
+
+resource "proxmox_virtual_environment_vm" "docker_vm" {
+  name      = "docker"
+  node_name = var.proxmox_node_name
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 4
+  }
+
+  memory {
+    dedicated = 16384
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    import_from  = proxmox_virtual_environment_download_file.debian_12_genericcloud.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 50
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.docker_cloud_init.id
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  serial_device {
+    device = "socket"
+  }
+  vga {
+    type = "serial0"
+  }
+}
+
+output "docker_vm_id" {
+  value = proxmox_virtual_environment_vm.docker_vm.id
+}
+
+output "docker_vm_ip" {
+  value = proxmox_virtual_environment_vm.docker_vm.ipv4_addresses[1][0]
 }
