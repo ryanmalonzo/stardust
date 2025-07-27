36705064d72399cd251ceef2959b299b3719d988
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-07-27 13:34:41 +0200

    feat(terraform): add scaleway resources and outputs for pangolin_vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ output "docker_vm_id" {
 output "docker_vm_ip" {
   value = proxmox_virtual_environment_vm.docker_vm.ipv4_addresses[1][0]
 }
+
+output "pangolin_vm_id" {
+  value = scaleway_instance_server.pangolin_vm.id
+}
+
+output "pangolin_vm_ip" {
+  value = scaleway_instance_server.pangolin_vm.public_ips[0].address
+}
--- terraform/scaleway.tf
+++ terraform/scaleway.tf
@@ -1,1 +1,48 @@
+provider "scaleway" {
+  zone   = var.scaleway_zone
+  region = var.scaleway_region
+}
 
+resource "scaleway_instance_security_group" "www" {
+  project_id              = var.scaleway_project_id
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+
+  inbound_rule {
+    action = "accept"
+    port   = "80"
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "443"
+  }
+
+  # Wireguard
+  inbound_rule {
+    action   = "accept"
+    port     = "51820"
+    protocol = "UDP"
+  }
+}
+
+resource "scaleway_instance_ip" "pangolin_vm_ip" {
+  project_id = var.scaleway_project_id
+  type       = "routed_ipv6"
+}
+
+resource "scaleway_instance_server" "pangolin_vm" {
+  project_id = var.scaleway_project_id
+
+  type      = "STARDUST1-S"
+  image     = "debian_bookworm"
+  protected = true
+
+  root_volume {
+    delete_on_termination = false
+  }
+
+  ip_id = scaleway_instance_ip.pangolin_vm_ip.id
+
+  security_group_id = scaleway_instance_security_group.www.id
+}
