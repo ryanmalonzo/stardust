56f0a6a301e2137ab09f7394185f44448df7a976
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-07-27 23:28:38 +0200

    feat(terraform): move disk to zfs pool and increase disk size for docker vm

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -10,6 +10,13 @@ data "proxmox_virtual_environment_datastores" "terraform" {
   }
 }
 
+data "proxmox_virtual_environment_datastores" "zfs" {
+  node_name = var.proxmox_node_name
+  filters = {
+    id = "tank"
+  }
+}
+
 resource "proxmox_virtual_environment_download_file" "debian_12_genericcloud" {
   content_type = "import"
   datastore_id = data.proxmox_virtual_environment_datastores.terraform.datastores[0].id
@@ -51,12 +58,12 @@ resource "proxmox_virtual_environment_vm" "docker_vm" {
   }
 
   disk {
-    datastore_id = "local-lvm"
+    datastore_id = data.proxmox_virtual_environment_datastores.zfs.datastores[0].id
     import_from  = proxmox_virtual_environment_download_file.debian_12_genericcloud.id
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
-    size         = 50
+    size         = 1024
   }
 
   initialization {
