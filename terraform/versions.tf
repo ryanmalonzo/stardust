ad73dc75734b4d6bfbb3dec5397a001ed49a8a3f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-07-27 11:45:24 +0200

    chore(terraform): reorder local provider definition in versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = "~> 1.12"
 
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.80.0"
-    }
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.3"
     }
   }
 }
