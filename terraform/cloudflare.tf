97bc8632758aa4f52158d355b87997e3a4cce4da
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-07-28 07:22:11 +0200

    refactor(terraform): use loop for dns records

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,17 +1,30 @@
-resource "cloudflare_dns_record" "main" {
-  zone_id = var.cloudflare_zone_id
-  name    = "ryanmalonzo.com"
-  ttl     = 1
-  type    = "A"
-  content = var.pangolin_ip
-  proxied = false
+locals {
+  dns_records = [
+    {
+      name    = "ryanmalonzo.com"
+      type    = "A"
+      content = var.pangolin_ip
+    },
+    {
+      name    = "pangolin.ryanmalonzo.com"
+      type    = "CNAME"
+      content = "ryanmalonzo.com"
+    },
+    {
+      name    = "portainer.ryanmalonzo.com"
+      type    = "CNAME"
+      content = "ryanmalonzo.com"
+    }
+  ]
 }
 
-resource "cloudflare_dns_record" "pangolin" {
+resource "cloudflare_dns_record" "dns_records" {
+  for_each = { for rec in local.dns_records : rec.name => rec }
+
   zone_id = var.cloudflare_zone_id
-  name    = "pangolin.ryanmalonzo.com"
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.content
   ttl     = 1
-  type    = "CNAME"
-  content = "ryanmalonzo.com"
   proxied = false
 }
